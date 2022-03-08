25ad4491633a3930ef722e7c4a7dfdff92c5b36d
px14 <px14@duke.edu>
2022-03-08 11:06:24 -0500

    completed 6017

---
+++ weekly_challenge/week283/6017.py
@@ -0,0 +1,30 @@
+from typing import List
+from math import inf
+
+def sum_range(start, end):
+    return (start + end) * (end - start + 1) // 2
+
+
+# 边界条件难以处理？手动设置极端值作为边界
+class Solution:
+    def minimalKSum(self, nums: List[int], k: int) -> int:
+        nums = sorted(nums)
+        nums = [0] + nums + [inf]
+        diffs = [nums[i] - nums[i-1] - 1 for i in range(1, len(nums))]
+
+        res = 0
+        i = 0
+        while k > 0:
+            if diffs[i] > 0:
+                res += sum_range(nums[i]+1, nums[i] + min(k, diffs[i]))
+                k -= diffs[i]
+            i += 1
+        return res
+
+
+if __name__ == '__main__':
+    s = Solution()
+    ans = s.minimalKSum([4, 1, 25, 10, 25], 2)
+    print(ans)
+    ans = s.minimalKSum([5, 6], 6)
+    print(ans)
