9eb872a556f919fd975f01856051ece45f26e911
px14 <px14@duke.edu>
2022-03-08 11:06:14 -0500

    completed 6016

---
+++ weekly_challenge/week283/6016.py
@@ -0,0 +1,19 @@
+def pos_to_cr(pos):
+    c = ord(pos[0]) - ord('A')
+    r = int(pos[1])
+    return c, r
+
+letters = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'.split(' ')
+
+
+class Solution:
+    def cellsInRange(self, s: str):
+        pos1, pos2 = s.split(":")
+        c1, r1 = pos_to_cr(pos1)
+        c2, r2 = pos_to_cr(pos2)
+        return [letters[c] + str(r) for c in range(c1, c2+1) for r in range(r1, r2+1)]
+
+if __name__ == '__main__':
+    s = Solution()
+    ans = s.cellsInRange("A1:F1")
+    print(ans)
