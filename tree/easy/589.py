cff9a502eb532c0d72ebda1bfe47bdfa198f56ae
px14 <px14@duke.edu>
2022-03-09 11:24:33 -0500

    completed 589

---
+++ tree/easy/589.py
@@ -0,0 +1,23 @@
+from typing import List
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+
+class Solution:
+
+    def pre_traverse(self, root, ans):
+        if not root:
+            return
+        ans.append(root.val)
+        for child in root.children:
+            self.pre_traverse(child, ans)
+
+    def preorder(self, root: 'Node') -> List[int]:
+        ans = []
+        self.pre_traverse(root, ans)
+        return ans
