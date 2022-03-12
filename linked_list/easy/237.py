8b6cec2af839d305fb7db68df3c2a466bc3bf305
px14 <px14@duke.edu>
2022-03-11 21:07:16 -0500

    completed 237

---
+++ linked_list/easy/237.py
@@ -0,0 +1,13 @@
+
+# 借用评论区一句话：如何让自己在世界上消失，但又不死？ 
+# 将自己完全变成另一个人，再杀了那个人就行了。
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        target = node.next
+        node.val = target.val
+        node.next = target.next
+        target.next = None
