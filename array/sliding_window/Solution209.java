b3d5f774faaeeb054617d48fa61bdac054f58b24
px14 <px14@duke.edu>
2022-04-04 10:49:09 -0400

    completed 209 in java

---
+++ array/sliding_window/Solution209.java
@@ -0,0 +1,28 @@
+public class Solution209 {
+    public static int minSubArrayLen(int target, int[] nums) {
+        int start = 0;
+        int sum = 0;
+        Integer minLen = Integer.MAX_VALUE;
+        for (int end = 0; end < nums.length; end++){
+            sum += nums[end];
+
+            // update lower boundary of window
+            // if sum of subArray after removing the first element 
+            // is still greater than or equal to target, remove the first element  
+            while(start < end && sum - nums[start] >= target) {
+                sum -= nums[start];  // update status first
+                start += 1;          // then renew lower boundary
+            }
+
+            // when to update the result
+            if (sum >= target){
+                minLen = Math.min(end - start + 1, minLen);
+            }
+        }
+        return (minLen < Integer.MAX_VALUE)? minLen:0;
+    }
+    public static void main(String[] args){
+        int[] nums = {2, 3, 1, 2, 4, 3};
+        int ans = minSubArrayLen(7, nums);
+    }
+}
